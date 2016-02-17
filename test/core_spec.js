17da4c3329c85bbc0d432a027bedb00e71ecb9df
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-18 00:06:02 +0100

    Allow entries to be passed as array

--- test/core_spec.js
+++ test/core_spec.js
@@ -7,7 +7,7 @@ describe('application logic', () => {
   describe('set entries', () => {
     it('adds the entries to the state', () => {
       const state = Map();
-      const entries = List.of('Trainspotting', '28 Days Later');
+      const entries = ['Trainspotting', '28 Days Later'];
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 Days Later')
