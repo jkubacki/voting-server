a432e2c18a39046c32402d905855217c96ceb7ae
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-22 23:32:43 +0100

    Add handling NEXT action

--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,8 @@ export default function reducer(state, action) {
   switch (action.type) {
   case 'SET_ENTRIES':
     return setEntries(state, action.entries);
+  case 'NEXT':
+    return next(state);
   }
   return state;
 }
