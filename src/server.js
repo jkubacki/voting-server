7aaa094ad2da1da2811fd3cdec8221df00e47f7b
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-23 21:20:27 +0100

    Emit state on client connection

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(
-    () => io.emit('state', store.getState().toJS());
+    () => io.emit('state', store.getState().toJS())
   );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS())
+  });
 }
