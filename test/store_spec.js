c62aadcfb8896d56912997bdc58828b2c7a952c2
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-23 01:02:15 +0100

    Add spec for redux store

---
+++ test/store_spec.js
@@ -0,0 +1,14 @@
+import makeStore from '../src/store'
+
+describe('store', () => {
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(Map());
+
+    let action = {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']};
+    store.dispatch(action);
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    }));
+  });
+});
